dee3969319fac0945f0ce580c2a36e9cc2ad617b
Kir Proto Q <kir.proto@ya.ru>
2022-01-19 08:50:55 +0500

    incremented background color change

--- Light_2022/ViewController.swift
+++ Light_2022/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = false
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+//        if lightOn == true {
+//            lightOn = false
+//        } else {
+//            lightOn = true
+//        }
+        lightOn = !lightOn
+        
+        updateUI()
+    }
+        
+        func updateUI() {
+            view.backgroundColor = lightOn ? .red : .cyan
+            
+//            if lightOn {
+//                view.backgroundColor = UIColor.white
+//            } else {
+//                view.backgroundColor = UIColor.black
+//            }
+        }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
     }
 
 
-}
 
+
+}
